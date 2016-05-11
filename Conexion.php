dc267ce3e1b6d3634367ad14e41c396d7403e5b5
juan Rdgz <juanirodrigueza@gmail.com>
2016-05-11 03:46:11 +0000

    das

--- Conexion.php
+++ Conexion.php
@@ -1,9 +1,9 @@
 <?php
 // datos para la conexion a mysql
-define('DB_SERVER','localhost');
+define('DB_SERVER','127.0.0.1');
 define('DB_NAME','IEC');
-define('DB_USER','root');
-define('DB_PASS','');
+define('DB_USER','perickles');
+define('DB_PASS','perickles');
 $con = mysql_connect(DB_SERVER,DB_USER,DB_PASS);
 mysql_select_db(DB_NAME,$con);
 ?>

79561725c65be1846d5d5c2ae13ea9f1c9e5edad
source_server <source_server@example.com>
2016-05-11 21:59:06 +0000

    modify workspace

--- Conexion.php
+++ Conexion.php
@@ -1,11 +1,5 @@
 <?php
 // datos para la conexion a mysql
-// Using PDO_MySQL (connecting from App Engine)
-$db = new pdo(
-  'mysql:unix_socket=/cloudsql/serviciosnubetec:basededatosiec',
-  'root',  // username
-  'toor'       // password
-);
 
 // Using mysqli (connecting from App Engine)
 $sql = new mysqli(

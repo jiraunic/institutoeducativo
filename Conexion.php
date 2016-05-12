aa743eb52c0889562d0764ab562eacb5b883dd81
juan Rdgz <juanirodrigueza@gmail.com>
2016-05-12 01:31:07 +0000

    edicion de cerfyxz: calando la conexion a la base de datos

--- Conexion.php
+++ Conexion.php
@@ -1,4 +1,22 @@
 <?php
+/*$sql = new mysqli(
+  null, // host
+  'root', // username
+  'toor',     // password
+  'iec', // database name
+  null,
+  '/cloudsql/serviciosnubetec:basededatosiec'
+  );
+*/
+
+// Using PDO_MySQL (connecting from App Engine)
+$db = new pdo(
+  'mysql:unix_socket=/cloudsql/serviciosnubetec:basededatosiec',
+  'root',  // username
+  'toor'       // password
+);
+
+// Using mysqli (connecting from App Engine)
 $sql = new mysqli(
   null, // host
   'root', // username
@@ -6,6 +24,12 @@ $sql = new mysqli(
   'iec', // database name
   null,
   '/cloudsql/serviciosnubetec:basededatosiec'
+  );
+
+// Using MySQL API (connecting from App Engine)
+$conn = mysql_connect(':/cloudsql/serviciosnubetec:basededatosiec',
+  'root', // username
+  'toor'      // password
   );
 
 
